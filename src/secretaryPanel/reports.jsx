4292f785514545604ea0792b4cee9c8e07da1a4a
Kyle Casingal <you@example.com>
2022-11-25 07:08:53 +0800

    reports

---
+++ src/secretaryPanel/reports.jsx
@@ -0,0 +1,23 @@
+export default function Reports() {
+  return (
+    <>
+      <div className="reportsContainer">
+        <div className="buttonArea">
+          <button type="submit" className="btnSubmitReg">
+            Amenity Renting
+          </button>
+          <button type="submit" className="btnSubmitReg">
+            Paid Monthly Dues
+          </button>
+          <button type="submit" className="btnSubmitReg">
+            Unpaid Dues
+          </button>
+          <button type="submit" className="btnSubmitReg">
+            Audit Trail
+          </button>
+        </div>
+        
+      </div>
+    </>
+  );
+}
