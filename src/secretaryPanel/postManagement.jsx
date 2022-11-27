82d264cfd2c81ef7b6507016d9d6348bd2c45279
Kyle Casingal <you@example.com>
2022-11-28 01:23:43 +0800

    added complaint modal, post management

---
+++ src/secretaryPanel/postManagement.jsx
@@ -0,0 +1,197 @@
+import Topbar from "../topbar/TopBar";
+import Footer from "../footer/Footer";
+import "./secretaryPanel.css";
+
+export default function PostManagement() {
+    return (
+        <>
+            <label className="inboxTitle">Awaiting Approval</label>
+            <div className="inboxContainer">
+              <table className="tblMessage">
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+              </table>
+            </div>
+            
+            <label className="inboxTitle">Reported Posts</label>
+            <div className="inboxContainer">
+              <table className="tblMessage">
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+                <tr className="trInbox">
+                  <td className="msgSender">Jane Doe</td>
+                  <td className="msgDesc">
+                    "Neque porro quisquam est qui dolorem ipsum quia dolor sit
+                    amet, consectetur, adipisci velit sdasdasdasdsadas"
+                  </td>
+                  <td className="msgTime">9:00 pm</td>
+                </tr>
+              </table>
+            </div>
+        </>
+    );
+}
