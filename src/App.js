dd18e0597e4bf216ede6d58bcfa3de20680213c5
Kyle Casingal <kylecasingal36@gmail.com>
2022-12-04 05:14:05 +0800

    reports addition

--- src/App.js
+++ src/App.js
@@ -8,21 +8,34 @@ import AmenitiesGuest from "./amenitiesGuest/amenitiesGuest";
 import TreasurerPanel from "./treasurerPanel/treasurerPanel";
 import MemberPanel from "./memberPanel/memberPanel";
 import AdminPanel from "./adminPanel/adminPanel";
+import ReportAmenities from "./reports/reportAmenities";
+import ReportMonthlyDues from "./reports/reportMonthlyDues";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+
+          {/* BEFORE LOGIN */}
           <Route path="/" element={<Landing />}></Route>
           <Route path="/Register" element={<Register />}></Route>
           <Route path="/Login" element={<Login />}></Route>
+          <Route path="/AmenitiesGuest" element={<AmenitiesGuest />}></Route>
+
+          {/* MAIN CONTENT */}
           <Route path="/BlogHome" element={<BlogHome />}></Route>
           <Route path="/Amenities" element={<Amenities />}></Route>
-          <Route path="/AmenitiesGuest" element={<AmenitiesGuest />}></Route>
-          <Route path="/TreasurerPanel" element={<TreasurerPanel />}></Route>
           <Route path="/MemberPanel" element={<MemberPanel />}></Route>
+          
+          {/* SYSTEM ACCOUNT PANELS */}
+          <Route path="/TreasurerPanel" element={<TreasurerPanel />}></Route>
           <Route path="/AdminPanel" element={<AdminPanel />}></Route>
+
+          {/* REPORTS */}
+          <Route path="/ReportAmenities" element={<ReportAmenities />}></Route>
+          <Route path="/ReportMonthlyDues" element={<ReportMonthlyDues />}></Route>
+
         </Routes>
       </Router>
     </div>
--- src/adminPanel/reports.jsx
+++ src/adminPanel/reports.jsx
@@ -1,20 +1,116 @@
+import ReportAmenities from "../reports/reportAmenities";
+import ReportAuditTrail from "../reports/reportAuditTrail";
+import ReportBilling from "../reports/reportBilling";
+import ReportMonthlyDues from "../reports/reportMonthlyDues";
+
 export default function Reports() {
+
+  function Print(divName){
+    var printContents = document.getElementById(divName).innerHTML;
+			var originalContents = document.body.innerHTML;
+
+			document.body.innerHTML = printContents;
+
+			window.print();
+
+			document.body.innerHTML = originalContents;
+  }
+  
+  function showReportAmenities() {
+    var reportAmenities = document.getElementById("reportAmenities");
+    var reportMonthlyDues = document.getElementById("reportMonthlyDues");
+    var reportBilling = document.getElementById("reportBilling");
+    var reportAuditTrail = document.getElementById("reportAuditTrail");
+    
+
+    reportAmenities.style.display = "block";
+    reportMonthlyDues.style.display = "none";
+    reportBilling.style.display = "none";
+    reportAuditTrail.style.display = "none";
+    
+   }
+
+   function showReportMonthlyDues() {
+    var reportAmenities = document.getElementById("reportAmenities");
+    var reportMonthlyDues = document.getElementById("reportMonthlyDues");
+    var reportBilling = document.getElementById("reportBilling");
+    var reportAuditTrail = document.getElementById("reportAuditTrail");
+    
+
+    reportAmenities.style.display = "none";
+    reportMonthlyDues.style.display = "block";
+    reportBilling.style.display = "none";
+    reportAuditTrail.style.display = "none";
+    
+   }
+
+   function showReportBilling() {
+    var reportAmenities = document.getElementById("reportAmenities");
+    var reportMonthlyDues = document.getElementById("reportMonthlyDues");
+    var reportBilling = document.getElementById("reportBilling");
+    var reportAuditTrail = document.getElementById("reportAuditTrail");
+    
+
+    reportAmenities.style.display = "none";
+    reportMonthlyDues.style.display = "none";
+    reportBilling.style.display = "block";
+    reportAuditTrail.style.display = "none";
+    
+   }
+
+   function showReportAuditTrail() {
+    var reportAmenities = document.getElementById("reportAmenities");
+    var reportMonthlyDues = document.getElementById("reportMonthlyDues");
+    var reportBilling = document.getElementById("reportBilling");
+    var reportAuditTrail = document.getElementById("reportAuditTrail");
+    
+
+    reportAmenities.style.display = "none";
+    reportMonthlyDues.style.display = "none";
+    reportBilling.style.display = "none";
+    reportAuditTrail.style.display = "block";
+    
+   }
+
+
   return (
     <>
-      <div className="reportsContainer">
+      <div className="reportsModule">
         <div className="buttonArea">
-          <button type="submit" className="btnSubmitReg">
+          <button type="submit" className="btnSubmitReg" onClick={()=>{showReportAmenities()}}>
             Amenity Renting
           </button>
-          <button type="submit" className="btnSubmitReg">
-            Paid Monthly Dues
+          <button type="submit" className="btnSubmitReg" onClick={()=>{showReportMonthlyDues()}}>
+            Paid Dues
           </button>
-          <button type="submit" className="btnSubmitReg">
-            Unpaid Dues
+          <button type="submit" className="btnSubmitReg" onClick={()=>{showReportBilling()}}>
+            Billing 
           </button>
-          <button type="submit" className="btnSubmitReg">
+          <button type="submit" className="btnSubmitReg" onClick={()=>{showReportAuditTrail()}}>
             Audit Trail
           </button>
+          <button type="submit" className="btnPrint" onClick={()=>{Print("reportContainer")}}>
+            Print
+          </button>
+        </div>
+        <div className="reportContainer" id="reportContainer">
+
+          <div className="reportAmenities" id="reportAmenities">
+            <ReportAmenities />
+          </div>
+
+          <div className="reportMonthlyDues" id="reportMonthlyDues">
+            <ReportMonthlyDues />
+          </div>
+
+          <div className="reportBilling" id="reportBilling">
+            <ReportBilling />
+          </div>
+
+          <div className="reportAuditTrail" id="reportAuditTrail">
+            <ReportAuditTrail />
+          </div>
+          
         </div>
       </div>
     </>
